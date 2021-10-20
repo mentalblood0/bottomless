f5f2f8e84988bd5fa34a383e3328c8ecb1ae6790
Нечепоренко Степан Юрьевич <necheporenko_s_iu@goznak.ru>
2021-10-20 11:28:52 +0000

    added Lock class (works with RedisInterface)

---
+++ CRL/CRL/Lock.py
@@ -0,0 +1,40 @@
+from time import sleep
+
+from . import RedisInterface
+
+
+
+class Lock:
+
+	locks_path = ['_RedisInterface_locks']
+
+	def __init__(self, interface):
+		self.lock_interface = RedisInterface(
+			db=interface.db, 
+			path=self.locks_path + interface.path
+		)
+
+	def acquire(self):
+		self.lock_interface = 1
+	
+	def isAcquired(self):
+		return self.lock_interface()
+	
+	def release(self):
+		self.lock_interface = 0
+
+	def __enter__(self):
+
+		while self.isAcquired():
+			sleep(0.1)
+		self.acquire()
+		
+		return self
+	
+	def __exit__(self, exc_type, exc_value, traceback):
+		self.release()
+
+
+
+import sys
+sys.modules[__name__] = Lock
--- CRL/CRL/RedisInterface.py
+++ CRL/CRL/RedisInterface.py
@@ -1,4 +1,4 @@
-from . import RedisInterfaceIterator
+from . import RedisInterfaceIterator, Lock
 
 
 
