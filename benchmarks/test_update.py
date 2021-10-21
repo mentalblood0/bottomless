f93d6e40488549605a58bbd6cf64259025e3e0ab
necheporenko_s_iu <neceporenkostepan@gmail.com>
2021-10-21 12:34:09 +0300

    added realistic .update test

--- benchmarks/test_update.py
+++ benchmarks/test_update.py
@@ -1,5 +1,8 @@
 import time
+from typing import overload
+import uuid
 from redis import Redis
+from datetime import datetime
 
 from tests import config
 from CRL import RedisInterface
@@ -9,7 +12,7 @@ from CRL import RedisInterface
 db = Redis.from_url(config['db']['url'])
 
 
-def test_update():
+def test_simple():
 
 	interface = RedisInterface(db)
 	interface.clear()
@@ -27,46 +30,32 @@ def test_update():
 	assert overall < 1
 
 
-def test_call():
+def test_realistic():
 
 	interface = RedisInterface(db)
 	interface.clear()
-	
-	keys_number = 10 ** 3
-	interface |= {
-		i+1: i+1
-		for i in range(keys_number)
-	}
 
-	n = 10
+	n = 20 * 5
 	start = time.time()
+
 	for i in range(n):
-		interface()
+
+		while True:
+			id = uuid.uuid4().hex
+			if not id in interface['sessions']:
+				break
+		
+		interface['sessions'][id] = {
+			'id': id,
+			'name': i,
+			'opened': 1,
+			'datetime': str(datetime.now()), 
+			'state': 'new',
+			'commandCount': 0
+		}
+	
 	end = time.time()
 
 	overall = end - start
 
-	assert overall < 1
-
-
-def test_keys():
-
-	interface = RedisInterface(db)
-	interface.clear()
-	
-	
-	keys_number = 10 ** 3
-	interface |= {
-		i+1: i+1
-		for i in range(keys_number)
-	}
-
-	n = 2 * 10 ** 2
-	start = time.time()
-	for i in range(n):
-		interface.keys()
-	end = time.time()
-
-	overall = end - start
-
-	assert overall < 1
+	assert overall < 1 
