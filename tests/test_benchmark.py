818a5fea8950a8c5358f1e2c1ce7503a189b9c04
necheporenko_s_iu <neceporenkostepan@gmail.com>
2021-10-21 11:35:44 +0300

    added more benchmarks

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -9,7 +9,7 @@ from CRL import RedisInterface
 db = Redis.from_url(config['db']['url'])
 
 
-def test_basic():
+def test_update():
 
 	interface = RedisInterface(db)
 	interface.clear()
@@ -20,6 +20,51 @@ def test_basic():
 		i+1: i+1
 		for i in range(n)
 	}
+	end = time.time()
+
+	overall = end - start
+
+	assert overall < 1
+
+
+def test_call():
+
+	interface = RedisInterface(db)
+	interface.clear()
+	
+	keys_number = 10 ** 3
+	interface |= {
+		i+1: i+1
+		for i in range(keys_number)
+	}
+
+	n = 10
+	start = time.time()
+	for i in range(n):
+		interface()
+	end = time.time()
+
+	overall = end - start
+
+	assert overall < 1
+
+
+def test_keys():
+
+	interface = RedisInterface(db)
+	interface.clear()
+	
+	
+	keys_number = 10 ** 3
+	interface |= {
+		i+1: i+1
+		for i in range(keys_number)
+	}
+
+	n = 2 * 10 ** 2
+	start = time.time()
+	for i in range(n):
+		interface.keys()
 	end = time.time()
 
 	overall = end - start
